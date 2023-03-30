642782835c3a0b4ed56448b0d0b370eab4838da5
OmarAliSoliman <omarasmar9955@gmail.com>
2023-03-30 17:16:14 +0200

    responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,21 +32,21 @@ $(document).ready(function () {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
-            dots: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2.5,
           },
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
+            slidesToShow: 1.4,
           },
         },
       ],
